dfc96368c414e6cd33afd78240e0ab246c8a0a05
ckcomin <com20009@byui.edu>
2022-04-25 10:35:27 -0600

    first commit

---
+++ program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pro_classes
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
